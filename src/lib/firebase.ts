4aa5e1540fad8a16459b17896a91ef00debcad3d
Jmr Sai <jmrsai987@gmail.com>
2025-07-28 01:25:01 +0000

    Add // Import the functions you need from the SDKs you need import { ini

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYdUR6J67o4h5hfXspo6nfutDhtNjREQw",
+  authDomain: "medimind-ic8pk.firebaseapp.com",
+  projectId: "medimind-ic8pk",
+  storageBucket: "medimind-ic8pk.appspot.com",
+  messagingSenderId: "111683356544",
+  appId: "1:111683356544:web:84efe1810467520a24e365"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
